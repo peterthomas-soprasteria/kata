e07d3548ecebd9cc2844bcc76d2c03039257d756
peter-thomas_ssgia <peter.thomas@soprasteria.com>
2024-12-17 17:28:08 +0100

    my first commit

---
+++ src/main/java/com/peter/bnp/kata/KataApplication.java
@@ -0,0 +1,13 @@
+package com.peter.bnp.kata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/peter/bnp/kata/KataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peter.bnp.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
