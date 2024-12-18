00ddc1e80c8baef5723f2ec0f0d775ee3bfae9cb
peter-thomas_ssgia <peter.thomas@soprasteria.com>
2024-12-18 12:32:13 +0100

    feat: implement POST /auth/register integration

---
+++ src/main/java/com/peter/bnp/kata/dto/UserReponse.java
@@ -0,0 +1,4 @@
+package com.peter.bnp.kata.dto;
+
+public record UserReponse(String username) {
+}
--- src/main/java/com/peter/bnp/kata/model/User.java
+++ src/main/java/com/peter/bnp/kata/model/User.java
@@ -4,9 +4,11 @@ package com.peter.bnp.kata.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Data;
 
 @Entity
+@Table(name = "users")
 @Data
 public class User {
     @Id
