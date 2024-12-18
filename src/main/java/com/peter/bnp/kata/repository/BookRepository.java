7635d322515492ed1d58d17562a6218ac6cbd4c5
peter-thomas_ssgia <peter.thomas@soprasteria.com>
2024-12-18 16:59:02 +0100

    refactor: remove unused imports

--- src/main/java/com/peter/bnp/kata/repository/BookRepository.java
+++ src/main/java/com/peter/bnp/kata/repository/BookRepository.java
@@ -2,7 +2,6 @@ package com.peter.bnp.kata.repository;
 
 import com.peter.bnp.kata.model.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
