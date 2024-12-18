0a15a921d9f1d302db19a2367840539adcff6ef5
peter-thomas_ssgia <peter.thomas@soprasteria.com>
2024-12-18 14:55:13 +0100

    refactor: removed unused imports

--- src/main/java/com/peter/bnp/kata/util/JwtTokenUtil.java
+++ src/main/java/com/peter/bnp/kata/util/JwtTokenUtil.java
@@ -5,10 +5,8 @@ import io.jsonwebtoken.*;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Component;
-import io.jsonwebtoken.security.SignatureException;
 
 import javax.crypto.SecretKey;
-import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 @Component
--- src/test/java/com/peter/bnp/kata/JwtTokenUtilTest.java
+++ src/test/java/com/peter/bnp/kata/JwtTokenUtilTest.java
@@ -4,8 +4,6 @@ import com.peter.bnp.kata.config.JwtProperties;
 import com.peter.bnp.kata.util.JwtTokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
 
